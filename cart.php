7d6333a1146f467d47d2ca50f0a47bb99527f21c
GrunWong <44452927+GrunWong@users.noreply.github.com>
2021-02-15 01:45:29 +1300

    Create Cart class methods

--- cart.php
+++ cart.php
@@ -1,7 +1,46 @@
 <?php
 
 class Cart {
-    
+
+    private $overall;
+    private $products_in_cart;
+
+    public function __construct() {
+        $overall = 0;
+        $products_in_cart = [];
+    }
+
+    public function getOverall() {
+        return $overall;
+    }
+
+    public function getCart() {
+        return $products_in_cart;
+    }
+
+    public function addToCart($product, $price) {
+        for($i = 0; i < count($products_in_cart); $i++) {
+            // Checks if product already exists in cart or not, if not then add to cart
+            if(!strcmp($products_in_cart[$i]["name"])) {
+                $products_in_cart[$i]["quantity"]++;
+                $products_in_cart[$i]["total"] = number_format($products_in_cart[$i]["price"]), 2) * $products_in_cart[$i]["quantity"];
+            } else {
+                $products_in_cart[$i] = ["name" => $product, "price" => $price, "quantity" => 1, "total" = $price];
+            }
+        }
+    }
+
+    public function removeFromCart($product) {
+        for($i = 0; i < count($products_in_cart); $i++) {
+            if($products_in_cart[$i]["quantity"] > 1) {
+                $products_in_cart[$i]["quantity"]--;
+                $products_in_cart[$i]["total"] = number_format($products_in_cart[$i]["price"]), 2) * $products_in_cart[$i]["quantity"];
+            } else {
+                array_splice($products_in_cart, $i, 1);
+            }
+        }
+    }
+
 }
 
 ?>
